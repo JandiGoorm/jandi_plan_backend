b897ccd7dc432ccf538d4195f7e309a0452f0896
hhhan-jh <jeo4924@gmail.com>
2025-02-14 19:55:52 +0900

    Chore: IntelliJ 제안 사항 반영

--- src/main/java/com/jandi/plan_backend/config/SecurityConfig.java
+++ src/main/java/com/jandi/plan_backend/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.jandi.plan_backend.user.security.JwtAuthenticationFilter;
 import com.jandi.plan_backend.user.security.JwtTokenProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -49,7 +50,7 @@ public class SecurityConfig {
                 // 세션을 사용하지 않도록 설정 (stateless)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 // CSRF 보호 비활성화 (API 서버에서는 불필요)
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 // 요청에 대한 접근 권한 설정
                 .authorizeHttpRequests(authorize -> authorize
                         // 인증 없이 접근 가능한 엔드 포인트
