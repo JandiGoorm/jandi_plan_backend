b8937d39c15919155cd9da2dfb08e88db0f9c19f
hhhan-jh <jeo4924@gmail.com>
2025-02-17 21:17:55 +0900

    Feat: 공지사항 작성 API 추가

--- src/main/java/com/jandi/plan_backend/resource/controller/NoticeController.java
+++ src/main/java/com/jandi/plan_backend/resource/controller/NoticeController.java
@@ -1,8 +1,12 @@
 package com.jandi.plan_backend.resource.controller;
 
 import com.jandi.plan_backend.resource.dto.NoticeListDTO;
+import com.jandi.plan_backend.resource.dto.NoticeWritePostDTO;
+import com.jandi.plan_backend.resource.dto.NoticeWriteRespDTO;
 import com.jandi.plan_backend.resource.service.NoticeService;
+import com.jandi.plan_backend.user.security.JwtTokenProvider;
 import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -11,9 +15,12 @@ import java.util.Map;
 @RequestMapping("/api/notice")
 public class NoticeController {
     private final NoticeService noticeService;
+    private final JwtTokenProvider jwtTokenProvider; // 추가
 
-    public NoticeController(NoticeService noticeService) {
+
+    public NoticeController(NoticeService noticeService, JwtTokenProvider jwtTokenProvider) {
         this.noticeService = noticeService;
+        this.jwtTokenProvider = jwtTokenProvider;
     }
 
     /**페이지 단위로 공지글 리스트 조회*/
@@ -34,5 +41,23 @@ public class NoticeController {
                 "items", noticePage.getContent()   // 현재 페이지의 게시물 데이터
         );
     }
+
+    /** 공지사항 추가 API */
+    @PostMapping("/write/notice")
+    public ResponseEntity<?> writeNotice(
+            @RequestHeader("Authorization") String token, // 헤더의 Authorization에서 JWT 토큰 받기
+            @RequestBody NoticeWritePostDTO noticeDTO // JSON 형식으로 공지글 작성 정보 받기
+    ){
+
+        // Jwt 토큰으로부터 유저 이메일 추출
+        String jwtToken = token.replace("Bearer ", "");
+        String userEmail = jwtTokenProvider.getEmail(jwtToken);
+
+        // 공지글 저장 및 반환
+        NoticeWriteRespDTO savedNotice = noticeService.writeNotice(noticeDTO, userEmail);
+        return ResponseEntity.ok(savedNotice);
+    }
+
+
 }
 
