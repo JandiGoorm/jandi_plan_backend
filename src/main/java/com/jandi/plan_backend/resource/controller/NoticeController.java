a6ab46a5c026e4056ed28bf7b4747e32f746ae33
hhhan-jh <jeo4924@gmail.com>
2025-02-19 00:47:52 +0900

    Chore: 공지사항 조회 시 페이지네이션이 적용되지 않고 전체를 다 불러오도록 수정

--- src/main/java/com/jandi/plan_backend/resource/controller/NoticeController.java
+++ src/main/java/com/jandi/plan_backend/resource/controller/NoticeController.java
@@ -1,14 +1,14 @@
 package com.jandi.plan_backend.resource.controller;
 
-import com.jandi.plan_backend.resource.dto.NoticeListDTO;
 import com.jandi.plan_backend.resource.dto.NoticeReqDTO;
 import com.jandi.plan_backend.resource.dto.NoticeRespDTO;
+import com.jandi.plan_backend.resource.entity.Notice;
 import com.jandi.plan_backend.resource.service.NoticeService;
 import com.jandi.plan_backend.security.JwtTokenProvider;
-import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -23,22 +23,14 @@ public class NoticeController {
         this.jwtTokenProvider = jwtTokenProvider;
     }
 
-    /**페이지 단위로 공지글 리스트 조회*/
+    /** 공지글 전체 목록 조회 API */
     @GetMapping("/lists")
     public Map<String, Object> getAllNotices(
-            @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "10") int size
     ) {
-        Page<NoticeListDTO> noticePage = noticeService.getAllNotices(page, size);
+        List<Notice> noticeList = noticeService.getAllNotices();
 
         return Map.of(
-                "pageInfo", Map.of(
-                        "currentPage", noticePage.getNumber(),  // 현재 페이지 번호
-                        "currentSize", noticePage.getContent().size(), // 현재 페이지 리스트 개수
-                        "totalPages", noticePage.getTotalPages(),  // 전체 페이지 개수
-                        "totalSize", noticePage.getTotalElements() // 전체 게시물 개수
-                ),
-                "items", noticePage.getContent()   // 현재 페이지의 게시물 데이터
+                "items", noticeList   // 현재 페이지의 게시물 데이터
         );
     }
 
--- src/main/java/com/jandi/plan_backend/resource/service/NoticeService.java
+++ src/main/java/com/jandi/plan_backend/resource/service/NoticeService.java
@@ -1,17 +1,15 @@
 package com.jandi.plan_backend.resource.service;
 
-import com.jandi.plan_backend.resource.dto.NoticeListDTO;
 import com.jandi.plan_backend.resource.dto.NoticeReqDTO;
 import com.jandi.plan_backend.resource.dto.NoticeRespDTO;
 import com.jandi.plan_backend.resource.entity.Notice;
 import com.jandi.plan_backend.resource.repository.NoticeRepository;
 import com.jandi.plan_backend.user.entity.User;
 import com.jandi.plan_backend.util.ValidationUtil;
-import com.jandi.plan_backend.util.service.PaginationService;
-import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class NoticeService {
@@ -23,10 +21,11 @@ public class NoticeService {
         this.validationUtil = validationUtil;
     }
 
-    /** 공지글 목록 조회*/
-    public Page<NoticeListDTO> getAllNotices(int page, int size) {
-        long totalCount = noticeRepository.count();
-        return PaginationService.getPagedData(page, size, totalCount, noticeRepository::findAll, NoticeListDTO::new);
+    /**
+     * 공지글 목록 조회
+     */
+    public List<Notice> getAllNotices() {
+        return noticeRepository.findAll();
     }
 
     /** 공지글 작성 */
