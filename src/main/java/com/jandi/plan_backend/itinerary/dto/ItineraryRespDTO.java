0593299503c2befb4e72a416b250b3a68e33affc
김연재 <heroria0503@gmail.com>
2025-02-28 22:37:17 +0900

    FEAT: 일정에서 종료 시각 삭제

--- src/main/java/com/jandi/plan_backend/itinerary/dto/ItineraryRespDTO.java
+++ src/main/java/com/jandi/plan_backend/itinerary/dto/ItineraryRespDTO.java
@@ -13,19 +13,16 @@ public class ItineraryRespDTO {
     private Long placeId;
     private LocalDate date;
     private LocalTime startTime;
-    private LocalTime endTime;
     private String title;
     private Integer cost;
     private LocalDate createdAt;
 
     public ItineraryRespDTO(Itinerary itinerary) {
         this.itineraryId = itinerary.getItineraryId();
-        // trip 엔티티에서 tripId 추출 (Trip의 PK 타입이 Integer임)
         this.tripId = itinerary.getTrip().getTripId();
         this.placeId = itinerary.getPlaceId();
         this.date = itinerary.getDate();
         this.startTime = itinerary.getStartTime();
-        this.endTime = itinerary.getEndTime();
         this.title = itinerary.getTitle();
         this.cost = itinerary.getCost();
         this.createdAt = itinerary.getCreatedAt();
