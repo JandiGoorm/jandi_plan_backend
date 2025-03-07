9b8b9eb9ccaa3da821765f20b1c6f8f3f0978556
hhhan-jh <jeo4924@gmail.com>
2025-03-07 18:12:35 +0900

    Feat: 신고된 게시글 조회

--- src/main/java/com/jandi/plan_backend/commu/repository/CommunityReportedRepository.java
+++ src/main/java/com/jandi/plan_backend/commu/repository/CommunityReportedRepository.java
@@ -1,10 +1,23 @@
 package com.jandi.plan_backend.commu.repository;
 
+import com.jandi.plan_backend.commu.entity.Community;
 import com.jandi.plan_backend.commu.entity.CommunityReported;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CommunityReportedRepository extends JpaRepository<CommunityReported, Long> {
     Optional<CommunityReported> findByUser_userIdAndCommunity_postId(Integer userId, Integer postId);
+
+    // 신고된 이력이 있는 게시글을 중복없게 postId 별로 그룹화해서 신고 횟수를 계산한 뒤,
+    // 신고 횟수로 1차 정렬 후 동일 신고 횟수라면 postId 역순 정렬해서 반환
+    @Query("""
+    SELECT cr.community, COUNT(cr) FROM CommunityReported cr GROUP BY cr.community
+    ORDER BY COUNT(cr) DESC, cr.community.postId DESC
+    """)
+    Page<Object[]> findReportedCommunitiesWithCount(Pageable pageable);
 }
--- src/main/java/com/jandi/plan_backend/commu/service/ManageCommunityService.java
+++ src/main/java/com/jandi/plan_backend/commu/service/ManageCommunityService.java
@@ -1,7 +1,52 @@
 package com.jandi.plan_backend.commu.service;
 
+import com.jandi.plan_backend.commu.dto.CommunityListDTO;
+import com.jandi.plan_backend.commu.dto.CommunityReportedListDTO;
+import com.jandi.plan_backend.commu.entity.Community;
+import com.jandi.plan_backend.commu.entity.CommunityReported;
+import com.jandi.plan_backend.commu.repository.CommunityReportedRepository;
+import com.jandi.plan_backend.image.service.ImageService;
+import com.jandi.plan_backend.user.entity.User;
+import com.jandi.plan_backend.util.ValidationUtil;
+import com.jandi.plan_backend.util.service.PaginationService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ManageCommunityService {
+    private final ValidationUtil validationUtil;
+    private final CommunityReportedRepository communityReportedRepository;
+    private final ImageService imageService;
+
+    public ManageCommunityService(
+            ValidationUtil validationUtil,
+            CommunityReportedRepository communityReportedRepository,
+            ImageService imageService
+    ) {
+        this.validationUtil = validationUtil;
+        this.communityReportedRepository = communityReportedRepository;
+        this.imageService = imageService;
+    }
+
+    public Page<CommunityReportedListDTO> getReportedPosts(String userEmail, int page, int size) {
+        //유저 검증
+        User admin = validationUtil.validateUserExists(userEmail);
+        validationUtil.validateUserIsAdmin(admin);
+
+        long totalCount = communityReportedRepository.count(); // 전체 신고된 게시글 수
+        return PaginationService.getPagedData(page, size, totalCount,
+                communityReportedRepository::findReportedCommunitiesWithCount,  // 데이터 조회
+                ReportedObj -> {
+                    Community community = (Community) ReportedObj[0];  // 신고된 게시글
+                    Integer reportCount = ((Number) ReportedObj[1]).intValue();  // 신고 횟수
+                    return new CommunityReportedListDTO(community, imageService, reportCount);
+                }
+        );
+    }
+
 }
