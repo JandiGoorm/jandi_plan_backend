0d33573dd6b3dbc8aadf455b9d4aec381c8faa44
hhhan-jh <jeo4924@gmail.com>
2025-03-27 00:04:48 +0900

    Refactor: 코드 가독성을 위해 복잡한 메서드 분리
    1. 반복되는 유효성 검사를 별도의 메서드로 분리
    2. 실제 게시글 생성, 수정, 삭제 로직을 별도 메서드로 분리

--- src/main/java/com/jandi/plan_backend/commu/community/service/CommunityUpdateService.java
+++ src/main/java/com/jandi/plan_backend/commu/community/service/CommunityUpdateService.java
@@ -18,13 +18,16 @@ import com.jandi.plan_backend.image.service.InMemoryTempPostService;
 import com.jandi.plan_backend.user.entity.User;
 import com.jandi.plan_backend.util.CommunityUtil;
 import com.jandi.plan_backend.util.ValidationUtil;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class CommunityUpdateService {
     private final ValidationUtil validationUtil;
     private final CommunityRepository communityRepository;
@@ -38,52 +41,19 @@ public class CommunityUpdateService {
     private final CommentReportedRepository commentReportedRepository;
     private final CommentLikeRepository commentLikeRepository;
 
-    public CommunityUpdateService(
-            ValidationUtil validationUtil,
-            CommunityRepository communityRepository,
-            InMemoryTempPostService inMemoryTempPostService,
-            CommunityUtil communityUtil, ImageService imageService,
-            ImageRepository imageRepository,
-            CommunityReportedRepository communityReportedRepository,
-            CommunityLikeRepository communityLikeRepository,
-            CommentRepository commentRepository,
-            CommentReportedRepository commentReportedRepository,
-            CommentLikeRepository commentLikeRepository
-    ) {
-        this.validationUtil = validationUtil;
-        this.communityRepository = communityRepository;
-        this.inMemoryTempPostService = inMemoryTempPostService;
-        this.communityUtil = communityUtil;
-        this.imageService = imageService;
-        this.imageRepository = imageRepository;
-        this.communityReportedRepository = communityReportedRepository;
-        this.communityLikeRepository = communityLikeRepository;
-        this.commentRepository = commentRepository;
-        this.commentReportedRepository = commentReportedRepository;
-        this.commentLikeRepository = commentLikeRepository;
-    }
+    private static final ZoneId KST = ZoneId.of("Asia/Seoul");
 
     /** 최종 게시글 생성 (임시 postId(음수)를 실제 postId로 전환) */
+    @Transactional
     public CommunityRespDTO finalizePost(String userEmail, PostFinalizeReqDTO reqDTO) {
-        User user = validationUtil.validateUserExists(userEmail);
-        validationUtil.validateUserRestricted(user);
-        validationUtil.validateIsHashtagListValid(reqDTO.getHashtag());
+        User user = validateUserAndHashtag(userEmail, reqDTO.getHashtag());
         inMemoryTempPostService.validateTempId(reqDTO.getTempPostId(), user.getUserId());
 
-        Community community = new Community();
-        community.setUser(user);
-        community.setTitle(reqDTO.getTitle());
-        community.setContents(reqDTO.getContent());
-        community.setCreatedAt(LocalDateTime.now(ZoneId.of("Asia/Seoul")));
-        community.setLikeCount(0);
-        community.setCommentCount(0);
-        community.setPreview(communityUtil.getPreview(reqDTO.getContent())); // 미리보기 반영
-        community.setHashtags(reqDTO.getHashtag()); //해시태그 반영
-        communityRepository.save(community);
-
-        int realPostId = community.getPostId();
+        // 게시글 생성
+        Community community = createCommunity(reqDTO, user);
 
         // 임시 postId를 실제 postId로 업데이트
+        int realPostId = community.getPostId();
         imageService.updateTargetId("community", reqDTO.getTempPostId(), realPostId);
         inMemoryTempPostService.removeTempId(reqDTO.getTempPostId());
 
@@ -94,19 +64,14 @@ public class CommunityUpdateService {
     }
 
     /** 게시글 수정 */
+    @Transactional
     public CommunityRespDTO updatePost(CommunityReqDTO postDTO, Integer postId, String userEmail) {
+        User user = validateUserAndHashtag(userEmail, postDTO.getHashtag());
         Community post = validationUtil.validatePostExists(postId);
-        User user = validationUtil.validateUserExists(userEmail);
-        validationUtil.validateUserRestricted(user);
         validationUtil.validateUserIsAuthorOfPost(user, post);
-        validationUtil.validateIsHashtagListValid(postDTO.getHashtag());
 
-
-        post.setTitle(postDTO.getTitle());
-        post.setContents(postDTO.getContent());
-        post.setPreview(communityUtil.getPreview(postDTO.getContent())); // 미리보기 반영
-        post.setHashtags(postDTO.getHashtag()); // 해시태그 반영
-        communityRepository.save(post);
+        // 게시글 수정
+        updateCommunity(post, postDTO);
 
         // 게시글 수정 후, 사용되지 않는 이미지 삭제
         communityUtil.cleanupUnusedImages(post);
@@ -115,33 +80,66 @@ public class CommunityUpdateService {
     }
 
     /** 게시글 삭제 */
+    @Transactional
     public int deletePost(Integer postId, String userEmail) {
         Community post = validationUtil.validatePostExists(postId);
-
-        // 유저 검증
         User user = validationUtil.validateUserExists(userEmail);
         validationUtil.validateUserIsAuthorOfPost(user, post);
 
-        // 게시글의 신고 및 좋아요 정보 삭제
+        deleteCommunityData(postId, post);
+        communityRepository.delete(post);
+
+        return post.getCommentCount();
+    }
+
+    // 유저 및 해시태그 검증
+    private User validateUserAndHashtag(String userEmail, List<String> hashtags) {
+        User user = validationUtil.validateUserExists(userEmail);
+        validationUtil.validateUserRestricted(user);
+        validationUtil.validateIsHashtagListValid(hashtags);
+        return user;
+    }
+
+    // 게시글 생성
+    private Community createCommunity(PostFinalizeReqDTO reqDTO, User user) {
+        Community community = new Community();
+        community.setUser(user);
+        community.setTitle(reqDTO.getTitle());
+        community.setContents(reqDTO.getContent());
+        community.setCreatedAt(LocalDateTime.now(KST));
+        community.setLikeCount(0);
+        community.setCommentCount(0);
+        community.setPreview(communityUtil.getPreview(reqDTO.getContent())); // 미리보기 반영
+        community.setHashtags(reqDTO.getHashtag()); //해시태그 반영
+        communityRepository.save(community);
+
+        return community;
+    }
+
+    // 게시글 수정
+    private void updateCommunity(Community community, CommunityReqDTO postDTO) {
+        community.setTitle(postDTO.getTitle());
+        community.setContents(postDTO.getContent());
+        community.setPreview(communityUtil.getPreview(postDTO.getContent())); // 미리보기 반영
+        community.setHashtags(postDTO.getHashtag()); // 해시태그 반영
+        communityRepository.save(community);
+    }
+
+    // 게시글 삭제
+    private void deleteCommunityData(Integer postId, Community post) {
         communityReportedRepository.deleteAll(communityReportedRepository.findByCommunity_PostId(postId));
         communityLikeRepository.deleteAll(communityLikeRepository.findByCommunity(post));
 
-        // 게시글에 달린 모든 댓글 및 댓글의 신고/좋아요 정보 삭제
         List<Comment> comments = commentRepository.findByCommunity(post);
-        int commentsCount = comments.size();
         for (Comment comment : comments) {
             commentReportedRepository.deleteAll(commentReportedRepository.findByComment_CommentId(comment.getCommentId()));
             commentLikeRepository.deleteAll(commentLikeRepository.findByComment_CommentId(comment.getCommentId()));
         }
         commentRepository.deleteAll(comments);
 
-        // 게시글과 연결된 모든 이미지 삭제
         List<Image> images = imageRepository.findAllByTargetTypeAndTargetId("community", postId);
         for (Image image : images) {
             imageService.deleteImage(image.getImageId());
         }
-
-        communityRepository.delete(post);
-        return commentsCount;
     }
 }
