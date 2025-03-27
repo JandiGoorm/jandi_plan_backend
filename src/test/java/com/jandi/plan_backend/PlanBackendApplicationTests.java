773962208da05289a922a8eac4c4f2f2839894ef
김연재 <heroria0503@gmail.com>
2025-03-28 04:58:02 +0900

    FEAT: 테스트 코드

--- src/test/java/com/jandi/plan_backend/PlanBackendApplicationTests.java
+++ src/test/java/com/jandi/plan_backend/PlanBackendApplicationTests.java
@@ -2,6 +2,7 @@ package com.jandi.plan_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 class PlanBackendApplicationTests {
