9df242cf953b8b1f7e53601ec9d717531f76e037
김연재 <heroria0503@gmail.com>
2025-03-30 20:47:43 +0900

    FIX: 공지사항 최신순 정렬

--- src/main/java/com/jandi/plan_backend/resource/notice/service/NoticeQueryService.java
+++ src/main/java/com/jandi/plan_backend/resource/notice/service/NoticeQueryService.java
@@ -4,6 +4,7 @@ import com.jandi.plan_backend.resource.notice.dto.NoticeListDTO;
 import com.jandi.plan_backend.resource.notice.entity.Notice;
 import com.jandi.plan_backend.resource.notice.repository.NoticeRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,10 +17,9 @@ import java.util.stream.Collectors;
 public class NoticeQueryService {
     private final NoticeRepository noticeRepository;
 
-    /** 공지사항 목록 조회 */
-    @Transactional(readOnly = true)
+    /** 공지사항 목록 조회 (최신순으로 정렬) */
     public List<NoticeListDTO> getAllNotices() {
-        List<Notice> notices = noticeRepository.findAll();
+        List<Notice> notices = noticeRepository.findAll(Sort.by(Sort.Direction.DESC, "createdAt")); // 최신순 정렬
         return notices.stream()
                 .map(NoticeListDTO::new)
                 .collect(Collectors.toList());
